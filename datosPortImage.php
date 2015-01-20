cc39e9c8ffcd75404cecf06726e78d2cd20a2a0b
leonishan <leoni.3883@gmail.com>
2015-01-20 17:31:35 +0100

    Ports0.9

---
+++ datosPortImage.php
@@ -0,0 +1,41 @@
+<?php ob_start(); session_start(); require('routeros_api.class.php'); ?>
+
+<?php
+		$API = new routeros_api();
+		$IP = $_SESSION[ 'ip' ];
+		$user = $_SESSION[ 'user' ];
+		$password = $_SESSION[ 'password' ];
+		//Comprobamos conexion API
+		if ($API->connect($IP, $user, $password)) {
+
+		//Comprobamos interfaces
+		
+		$Ports = $API->comm("/interface/ethernet/print");
+		$numPorts = count($Ports);
+
+		//Modelo
+		$modeloCom = $API->comm("/system/routerboard/print");
+		$modelo=$modeloCom[0]['model'];
+		//Estado Link
+
+		$valoresPar= json_encode(range(0, $numPorts-1));
+		$valores = substr($valoresPar, 1, -1);
+
+
+		$API->write("/interface/ethernet/monitor",false);
+		$API->write("=numbers=".$valores,false);  
+		$API->write("=once=",true);
+		$READ = $API->read(false);
+		$statusPorts = $API->parse_response($READ);
+		$API->disconnect();}	
+				for ($cont = 0; $cont < $numPorts; $cont++){
+				
+				if($statusPorts[$cont]['status']=='link-ok'){
+					echo "<div class='etherGreen' id='etherGreen$cont'><img src='images/etherGreen.png'></div>";			
+					}
+				else if($statusPorts[$cont]['status']=='no-link'){			
+					}
+				
+				}
+				echo "<img src='images/$modelo.png'>";			
+?>
